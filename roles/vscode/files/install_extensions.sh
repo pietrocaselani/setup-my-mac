2bcc479a893c0817f8e7ed60055a22774859595d
Pietro Caselani <pc1992@gmail.com>
2019-07-09 19:26:00 -0300

    More VSCode configuration

--- roles/vscode/files/install_extensions.sh
+++ roles/vscode/files/install_extensions.sh
@@ -2,3 +2,5 @@
 
 code --install-extension misogi.ruby-rubocop
 code --install-extension svanimpe.stencil
+code --install-extension alex-munene.signed-dark-pro
+code --install-extension dbaeumer.vscode-eslint
