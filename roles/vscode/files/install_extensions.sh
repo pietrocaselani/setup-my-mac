61eb9c36a36ee50fec8e3360caaf3817641aa89a
Pietro Caselani <pc1992@gmail.com>
2019-07-23 10:58:05 -0300

    Uninstall custom VSCode theme

--- roles/vscode/files/install_extensions.sh
+++ roles/vscode/files/install_extensions.sh
@@ -2,5 +2,4 @@
 
 code --install-extension misogi.ruby-rubocop
 code --install-extension svanimpe.stencil
-code --install-extension alex-munene.signed-dark-pro
 code --install-extension dbaeumer.vscode-eslint
